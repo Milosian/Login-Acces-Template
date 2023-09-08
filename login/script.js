0e13ac24921ea0d99c03832d8895c9a41d48d2b3
Milosian <93874492+Milosian@users.noreply.github.com>
2023-09-08 09:27:47 +0200

    Add files via upload

--- login/script.js
+++ login/script.js
@@ -1,12 +1,13 @@
 const form = document.querySelector('.form');
-const footer = document.querySelector('.footer');
-    
+const username = document.querySelector('.user-name');
+const user_password = document.querySelector('.user-password');
 form.addEventListener('submit', e => {
     e.preventDefault();
     
     const data = new FormData(form);
     const name = data.get('name');
     const password = data.get('password');
-
-    footer.textContent = name  + " " + password;
+    
+    username.textContent = 'Username: ' + name;
+    user_password.textContent = 'Password: ' + password;
 });
